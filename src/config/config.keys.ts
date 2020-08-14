ba5d6dffdb1faf8d643ef80d2248bd812e0e9bc9
luster02 <bombolti_02@hotmail.com>
2020-08-14 12:19:25 -0500

    [ADD]: config module

---
+++ src/config/config.keys.ts
@@ -0,0 +1,10 @@
+export enum Configuration {
+  PORT = 'PORT',
+  HOST = 'HOST',
+  USERNAME = 'USERNAME',
+  PASSWORD = 'PASSWORD',
+  DATABASE = 'DATABASE',
+  JWT_SECRET = 'JWT_SECRET',
+  DB_TYPE = 'DB_TYPE',
+  DB_PORT = 'DB_PORT',
+}
