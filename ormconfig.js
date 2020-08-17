e5f3d815ed4d83bbcff6a3edadb8792f1f8c71c9
luster02 <bombolti_02@hotmail.com>
2020-08-17 16:52:44 -0500

    [FIX]: modify orm config file

---
+++ ormconfig.js
@@ -0,0 +1,13 @@
+module.exports = {
+  "type": process.env.DB_TYPE,
+  "username": process.env.USERNAME,
+  "password": process.env.PASSWORD,
+  "host": process.env.HOST,
+  "port": process.env.DB_PORT,
+  "database": process.env.DATABASE,
+  "entities": ["src/**/**/*.entity{.ts,.js}"],
+  "migrations": ["src/database/migrations/*{.ts,.js}"],
+  "cli": {
+    "migrationsDir": "src/database/migrations"
+  }
+}
