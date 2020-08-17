60b613b3a7dcd55432859680265d28013ecc8e2c
luster02 <bombolti_02@hotmail.com>
2020-08-16 22:59:41 -0500

    [ADD]: user detail repository

---
+++ src/modules/user/user.detail.repository.ts
@@ -0,0 +1,5 @@
+import { Repository, EntityRepository } from 'typeorm';
+import { UserDetails } from './user.details.entity';
+
+@EntityRepository(UserDetails)
+export class UserDetailRepository extends Repository<UserDetails> { }
