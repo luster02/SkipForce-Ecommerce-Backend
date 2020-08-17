65d1320da4c410570338dfe83d4b1a2389f4a7a8
luster02 <bombolti_02@hotmail.com>
2020-08-16 22:57:00 -0500

    [ADD]: open api

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api');
+  const options = new DocumentBuilder()
+    .setTitle('commerce backend')
+    .setDescription('open commerce backend')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(AppModule.port);
 }
