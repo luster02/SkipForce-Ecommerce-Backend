a8495adf375839f23104b27e10939c86c5caba25
luster02 <bombolti_02@hotmail.com>
2020-08-16 23:04:42 -0500

    [ADD]: user resolver

--- src/modules/user/user.resolver.ts
+++ src/modules/user/user.resolver.ts
@@ -1,10 +1,13 @@
-import { Resolver, Args, Query, Int } from '@nestjs/graphql'
+import { Resolver, Args, Query, Int, Mutation } from '@nestjs/graphql'
 import { User } from './user.entity'
-import { UserService } from './user.service'
 import { UseGuards } from '@nestjs/common'
+import { UserService } from './user.service'
 import { GqlAuthGuard } from '../auth/guards/graph.guard'
+import { MutationResult } from '../../graphql/interfaces'
+import { UserDetailDto } from './dto/user.detail.dto'
 
 @Resolver(of => User)
+@UseGuards(GqlAuthGuard)
 export class UserResolver {
     constructor(private readonly _userService: UserService) { }
 
@@ -13,9 +16,24 @@ export class UserResolver {
         return await this._userService.get(id)
     }
 
-    @UseGuards(GqlAuthGuard)
     @Query(returns => [User])
     async users(): Promise<User[]> {
         return await this._userService.getAll()
     }
+
+    @Mutation(returns => MutationResult)
+    async updateUser(
+        @Args('id', { type: () => Int }) id: number,
+        @Args('userData') userData: UserDetailDto
+    ): Promise<MutationResult> {
+        await this._userService.update(id, userData)
+        return { success: true }
+    }
+
+    @Mutation(returns => MutationResult)
+    async deleteUser(@Args('id', { type: () => Int }) id: number): Promise<MutationResult> {
+        await this._userService.delete(id)
+        return { success: true }
+    }
+
 }
