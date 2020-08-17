e4e43e3f1eec4245bae7cd9496888540b4aef1dc
luster02 <bombolti_02@hotmail.com>
2020-08-17 15:14:02 -0500

    [ADD]: import graphql modules

--- src/graphql/graphql.provider.ts
+++ src/graphql/graphql.provider.ts
@@ -1,12 +1,27 @@
 import { GraphQLModule } from '@nestjs/graphql'
 import { UserModule } from '../modules/user/user.module'
 import { RoleModule } from '../modules/role/role.module';
-import { AuthModule } from '../modules/auth/auth.module'
+import { AuthModule } from '../modules/auth/user/auth.module'
+import { ShopModule } from '../modules/shop/shop.module'
+import { ProductModule } from '../modules/product/product.module'
+import { OrderRepository } from '../modules/order/order.repository'
+import { GalleryModule } from '../modules/gallery/gallery.module'
+import { CustomerResolver } from '../modules/customer/customer.resolver'
+import { CartModule } from '../modules/cart/cart.module'
+import { CustomerAuthModule } from '../modules/auth/customer-auth/customer.auth.module'
+import { AddressModule } from '../modules/address/address.module'
 
 export const graphQLProvider = [
     GraphQLModule.forRoot({
         autoSchemaFile: 'schema.gql',
-        include: [UserModule, RoleModule, AuthModule],
+        include: [
+            UserModule, RoleModule,
+            AuthModule, ShopModule,
+            ProductModule, OrderRepository,
+            GalleryModule, CustomerResolver,
+            CartModule, CustomerAuthModule,
+            AddressModule
+        ],
         context: ({ req }) => ({ req })
     })
 ]
