d7963922bbf6a4fd5a1ad5928cd701187d161e1b
luster02 <bombolti_02@hotmail.com>
2020-08-16 23:00:32 -0500

    [FIX]: refactoring

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -3,13 +3,18 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserRepository } from './user.repository';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
-import { AuthModule } from '../auth/auth.module';
+import { AuthModule } from '../auth/user/auth.module';
 import { RoleRepository } from '../role/role.repository';
 import { UserResolver } from './user.resolver'
+import { UserDetailRepository } from './user.detail.repository'
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([UserRepository, RoleRepository]),
+    TypeOrmModule.forFeature([
+      UserRepository,
+      RoleRepository,
+      UserDetailRepository
+    ]),
     AuthModule,
   ],
   providers: [UserService, UserResolver],
