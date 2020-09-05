52d3ec6d5328ab177b36f31a6e066d95b3cf2c7e
luster02 <bombolti_02@hotmail.com>
2020-09-05 14:47:30 -0500

    [FIX]: remove files

--- src/modules/auth/guards/jwt.guard.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from "@nestjs/common";
-import { AuthGuard } from "@nestjs/passport";
-
-@Injectable()
-export class UserAuthGuard extends AuthGuard('UserStrategy') { }
-
-@Injectable()
-export class CustomerAuthGuard extends AuthGuard('CustomerStrategy') { }
