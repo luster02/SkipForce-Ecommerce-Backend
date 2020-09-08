e1ec5660b96df9fa1055d8d8ddfdc2d089f333d7
luster02 <bombolti_02@hotmail.com>
2020-09-08 18:31:30 -0500

    [FIX]: assets relations

--- src/modules/product/product.service.ts
+++ src/modules/product/product.service.ts
@@ -3,16 +3,12 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { ProductRepository } from './product.repository';
 import { Product } from './product.entity';
 import { ProductDto } from './dto/product.dto';
-import { AssetRepository } from '../asset/asset.repository';
-import { Asset } from '../asset/asset.entity';
 
 @Injectable()
 export class ProductService {
     constructor(
         @InjectRepository(ProductRepository)
-        private readonly _productRepository: ProductRepository,
-        @InjectRepository(AssetRepository)
-        private readonly _assetRepository: AssetRepository
+        private readonly _productRepository: ProductRepository
     ) { }
 
     async get(id: number): Promise<Product> {
@@ -43,31 +39,23 @@ export class ProductService {
         await this._productRepository.update(id, productData)
     }
 
-    async pushAssets(id: number, assets: any): Promise<void> {
+    async pushAssets(id: number, assets: number[]): Promise<void> {
         if (!id) throw new BadRequestException('product id must be sent');
         const product: any = await this._productRepository.findOne(id)
         if (!product) throw new NotFoundException();
-        const array: Asset[] = await this._assetRepository
-            .createQueryBuilder('asset')
-            .where('asset.id IN (:...id)', { id: assets.assets })
-            .getMany()
-        array.forEach((asset) => {
+        assets.forEach(async (asset: number) => {
             if (!product.assets.includes(asset)) {
-                product.assets.push(asset)
+                await this._productRepository.createQueryBuilder()
+                    .relation(Product, 'assets').of(product).add(asset)
             }
         })
-        await this._productRepository.save(product)
     }
 
     async pullAssets(id: number, assets: any): Promise<void> {
         if (!id) throw new BadRequestException('product id must be sent');
         const product: Product = await this._productRepository.findOne(id)
         if (!product) throw new NotFoundException();
-        const array: Asset[] = await this._assetRepository
-            .createQueryBuilder('asset')
-            .where('asset.id IN (:...id)', { id: assets.assets })
-            .getMany()
-        array.forEach((assetToRemove) => {
+        assets.forEach((assetToRemove) => {
             product.assets = product.assets.filter(asset => {
                 asset.id !== assetToRemove.id
             })
