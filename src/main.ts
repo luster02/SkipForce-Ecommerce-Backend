8794993ad8611eccc0c20b32e71f266c162b01c5
luster02 <bombolti_02@hotmail.com>
2020-10-19 20:20:08 -0500

    [FIX]: remove rest method

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,11 @@
 import { NestFactory } from '@nestjs/core';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import * as helmet from 'helmet';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const options = new DocumentBuilder()
-    .setTitle('commerce backend')
-    .setDescription('open commerce backend')
-    .setVersion('1.0')
-    .build();
-  const document = SwaggerModule.createDocument(app, options);
-  SwaggerModule.setup('api', app, document);
-  app.enableCors()
-  app.use(helmet())
+  //app.enableCors()
+  //app.use(helmet())
   await app.listen(AppModule.port);
 }
 bootstrap();
--- src/modules/asset/asset.controller.ts
+++ src/modules/asset/asset.controller.ts
@@ -1,10 +1,8 @@
 import {
-    Controller, Get, HttpCode,
-    Param, ParseIntPipe, Post,
-    UsePipes, ValidationPipe,
-    Delete, UseInterceptors, UploadedFile, UseGuards
+    Controller, HttpCode, Param,
+    ParseIntPipe, Post, UsePipes, ValidationPipe,
+    UseInterceptors, UploadedFile, UseGuards
 } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
 import { FileInterceptor } from '@nestjs/platform-express'
 import { AuthGuard } from '@nestjs/passport';
 import { AssetService } from './asset.service';
@@ -12,25 +10,10 @@ import { CustomResponse } from '../../interfaces/Response.interface';
 import { multerOptions } from '../../shared/multer.storage'
 
 
-@ApiTags('asset')
 @UseGuards(AuthGuard('UserStrategy'))
 @Controller('asset')
 export class AssetController {
     constructor(private readonly _assetService: AssetService) { }
-
-    @Get(':id')
-    @HttpCode(200)
-    async getAsset(@Param('id', ParseIntPipe) id: number): Promise<CustomResponse> {
-        const asset = await this._assetService.get(id)
-        return { ok: true, data: asset }
-    }
-
-    @Get('/all/:id')
-    @HttpCode(200)
-    async getAllProducts(@Param('id', ParseIntPipe) id: number): Promise<CustomResponse> {
-        const assets = await this._assetService.getAll(id)
-        return { ok: true, data: assets }
-    }
 
     @Post(':id')
     @UseInterceptors(FileInterceptor('file', multerOptions))
@@ -42,12 +25,5 @@ export class AssetController {
     ): Promise<CustomResponse> {
         const asset = await this._assetService.create(id, file.path)
         return { ok: true, data: asset }
-    }
-
-    @Delete(':id')
-    @HttpCode(200)
-    async deleteAsset(@Param('id', ParseIntPipe) id: number): Promise<CustomResponse> {
-        await this._assetService.delete(id)
-        return { ok: true, data: 'deleted' }
     }
 }
